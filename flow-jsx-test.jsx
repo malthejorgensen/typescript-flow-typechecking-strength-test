fbb9445c36cd9e632025f7162f42f051df989f3b
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2017-06-14 15:57:37 -0700

    Create flow-jsx-test.jsx

---
+++ flow-jsx-test.jsx
@@ -0,0 +1,81 @@
+// @flow
+
+import * as React from 'react'
+import type { Answer, Question, ReportGrade, Section } from 'models'
+
+const ReactToFeedbackQuestion = ({ idx, numQuestions, question, answer, name }: {
+  idx: number,
+  numQuestions: number,
+  question: Question,
+  answer: Answer,
+  name: string
+}) => {
+  const props = {
+    type: question.type,
+    question,
+    answer,
+    name,
+    numQuestions,
+    idx
+  }
+  return (
+    <div>
+      {answer.question.id}
+    </div>
+  )
+}
+
+const FeedbackSection = ({ section, answers }: { section: Section, answers: { [id: string]: Answer } }) => {
+  const questions = section.questions.map((question, idx) => {
+    return (<ReactToFeedbackQuestion
+      key={idx}
+      question={question}
+      idx={idx}
+      numQuestions={section.questions.length}
+      name={question.id}
+      answer={answers[question.id]}
+    />)
+  })
+  return (
+    <div>
+      <h2 className="pl3 fs-m color grey db">{section.title}</h2>
+      <div>
+        {questions}
+      </div>
+    </div>
+  )
+}
+
+const Feedback = ({ reportGrade }: { reportGrade: ReportGrade }) => {
+  let answers = {}
+  reportGrade.answers.map((a) => { answers[a.question.id] = a; return a })
+
+  // const reducerFunc = (
+  //   acc: Object,
+  //   {
+  //     question: { id },
+  //     answer
+  //   }: {
+  //     question: { id: string },
+  //     answer: string | number | boolean
+  //   }) => {
+  //   acc[id] = {
+  //     hoblgobl: id,
+  //     answer
+  //   }
+  //   return acc
+  // }
+  // const answers = reportGrade.answers.reduce(reducerFunc, {})
+  // const answers = {
+  //   hoblgobl: { answer: 'Hello', question: {id: 'argh'} }
+  // }
+
+  const feedbackSections = reportGrade.sections.map((section) => {
+    return (<FeedbackSection
+      key={section.id}
+      section={section}
+      answers={answers}
+    />)
+  })
+  return <div>{feedbackSections}</div>
+}
